da1054cf710c1055b299cae77c9ceffc12d0abb8
nansystem <sato171786@gmail.com>
2021-06-02 23:11:03 +0900

    feat: add breadcrumb

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="text-xs text-gray-600">
+    <ol
+      class="breadcrumb"
+      itemscope
+      itemtype="https://schema.org/BreadcrumbList"
+    >
+      <li
+        v-for="(b, i) in bs"
+        :key="b.link"
+        itemprop="itemListElement"
+        itemscope
+        itemtype="https://schema.org/ListItem"
+      >
+        <nuxt-link :key="b.link" itemprop="item" :to="b.link">
+          <span itemprop="name">{{ b.name }}</span></nuxt-link
+        >
+        <meta itemprop="position" :content="i + 1" />
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+
+export type Breadcrumb = {
+  name: string
+  link: string
+}
+
+type Props = {
+  breadcrumbs: Breadcrumb[]
+}
+
+export default defineComponent({
+  props: {
+    breadcrumbs: {
+      type: Array as PropType<Breadcrumb[]>,
+      default: () => [],
+    },
+  },
+  setup(props: Props) {
+    let bs = [{ name: '五島ブログ', link: '/' }]
+    if (props.breadcrumbs.length > 0) {
+      bs = bs.concat(props.breadcrumbs)
+    }
+    return {
+      bs,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.breadcrumb > li {
+  display: inline;
+}
+
+.breadcrumb > li + li::before {
+  margin: 0 5px;
+  content: '>';
+}
+</style>
--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -5,33 +5,33 @@
     <div
       class="max-w-screen-xl mx-auto p-4 lg:flex lg:flex-wrap justify-between"
     >
-      <div v-if="Object.keys(blog).length !== 0">
-        <div class="text-xs text-gray-600">
-          <nuxt-link to="/">五島ブログ</nuxt-link>
-          <template v-if="blog.categories && blog.categories.length > 0">
-            >
-            <nuxt-link :to="`/category/${blog.categories[0].id}`">{{
-              blog.categories[0].name
-            }}</nuxt-link>
-          </template>
-        </div>
-
-        <main class="main mt-4 lg:flex-1 lg:order-1">
-          <picture>
-            <img :src="`${blog.thumbnail.url}?q=80&ar=2:1&fit=crop&fp-y=0.5`" />
-          </picture>
-          <div v-if="blog.categories.length > 0" class="my-4 text-xs">
-            {{ blog.categories[0].name }}
+      <main class="main mt-4 lg:flex-1 lg:order-1">
+        <div v-if="Object.keys(blog).length !== 0">
+          <Breadcrumb :breadcrumbs="firstBreadcrumbs" />
+          <div class="mt-4">
+            <picture>
+              <img
+                :src="`${blog.thumbnail.url}?q=80&ar=2:1&fit=crop&fp-y=0.5`"
+              />
+            </picture>
+            <div v-for="b in breadcrumbs" :key="b.link" class="mt-4">
+              <nuxt-link
+                :to="b.link"
+                class="my-4 text-xs"
+                style="color: rgb(3, 103, 166)"
+                >{{ b.name }}</nuxt-link
+              >
+            </div>
+            <h1 class="text-2xl mt-4">{{ blog.title }}</h1>
+            <Time :datetime-text="blog.createdAt" />
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <div class="mt-4 post" v-html="body"></div>
           </div>
-          <h1 class="text-2xl mt-4">{{ blog.title }}</h1>
-          <Time :datetime-text="blog.createdAt" />
-          <!-- eslint-disable-next-line vue/no-v-html -->
-          <div class="mt-4 post" v-html="body"></div>
-        </main>
-      </div>
+        </div>
+      </main>
 
       <aside class="lg:w-1/3 xl:w-1/4 sidebar lg:flex-initial lg:order-2">
-        <Profile class="mt-8 mx-auto p-4 md:mx-4 lg:mx-6" />
+        <Profile class="mt-12 mx-auto p-4 md:mx-4 lg:mx-6" />
       </aside>
     </div>
   </div>
@@ -43,9 +43,11 @@ import {
   ref,
   useFetch,
   useContext,
+  computed,
 } from '@nuxtjs/composition-api'
 import { BlogResponse } from '@/types/blog'
 import marked from 'marked'
+import { Breadcrumb } from '~/components/Breadcrumb.vue'
 
 export default defineComponent({
   setup() {
@@ -63,7 +65,25 @@ export default defineComponent({
       blog.value = data
       body.value = marked(blog.value?.body || '')
     })
-    return { blog, body }
+    const breadcrumbs = computed<Breadcrumb[]>({
+      get: () => {
+        if (
+          blog.value.categories === undefined ||
+          blog.value.categories.length === 0
+        ) {
+          return []
+        }
+        return blog.value.categories!.map((c) => {
+          return { name: c.name, link: `/category/${c.id}` }
+        })
+      },
+      set: () => {},
+    })
+    const firstBreadcrumbs = computed<Breadcrumb[]>({
+      get: () => (breadcrumbs.value.length > 0 ? [breadcrumbs.value[0]] : []),
+      set: () => {},
+    })
+    return { blog, body, breadcrumbs, firstBreadcrumbs }
   },
 })
 </script>
