627f053d5f71c60b607b2efde6bf4c785deff131
nansystem <sato171786@gmail.com>
2022-10-09 17:36:36 +0900

    feat: プロフィールを可変にする

--- types/blog.ts
+++ types/blog.ts
@@ -13,8 +13,32 @@ export type BlogResponse = ContentResponse<{
   thumbnail?: Image
   body: string
   categories?: CategoryResponse[]
+  author: string[]
 }>
 
 export type BlogListResponse = ListContentsResponse<BlogResponse>
 
 export type CategoryListResponse = ListContentsResponse<CategoryResponse>
+
+export type Profile = {
+  name: string
+  title: string
+  description: string
+  profileImageId: string
+}
+
+export const nancy: Profile = {
+  name: 'なんしー',
+  title: 'プログラマー',
+  description:
+    '2021年に五島列島の福江島に夫婦で移住し、島で出会った犬と暮らしている。\n趣味はSUP、釣り、キャンプ、スイーツ巡り。',
+  profileImageId: 'e84e0262a3b543d38085143281b2360e',
+}
+
+export const mado: Profile = {
+  name: 'madoka♡',
+  title: '釣り人',
+  description:
+    '美味しいお魚をたくさん釣りたい。\n上手にお魚さばけるようになりたい。\nお魚の種類覚えたい。',
+  profileImageId: '86c7f6dfb49f48d69152654ad66ef5b8',
+}
